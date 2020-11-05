c6475a61c897474b1b20f0c789fd426bdb178457
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-11-05 16:45:57 -0500

    testing upload to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var notes;
+var util = require('util');
 
 //set up express app:
 var app = express();
@@ -13,6 +13,10 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
+
+const writefileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+let allNotes;
 
 //import db.json
 
@@ -35,7 +39,41 @@ app.get("/api/notes", function (req, res) {
         });
 });
 
+//saving posting notes
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    readFileAsync(path.join(__dirname, "Develop/db/db.json"), "utf8")
+        .then(function (data) {
+            allNotes = JSON.parse(data);
+            if (newNote.id || newNote.id === 0) {
+                let currNote = allNotes[newNote.id];
+                currNote.title = newNote.title;
+                currNote.text = newNote.text;
+            } else {
+                allNotes.push(newNote);
+            }
+            writefileAsync(path.join(__dirname, "Develop/db/db.json"), JSON.stringify(allNotes))
+                .then(function () {
+                    console.log("Wrote db.json");
+                })
+        });
+    res.json(newNote);
+});
 
+
+app.delete("/api/notes/:id", function (req, res) {
+    var id = req.params.id;
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function (data) {
+            allNotes = JSON.parse(data);
+            allNotes.splice(id, 1);
+            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
+                .then(function () {
+                    console.log("Deleted db.json");
+                })
+        });
+    res.json(id);
+});
 
 
 //server is listening
